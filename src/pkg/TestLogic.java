207b0ff38a5ab95b1f8a7fe8ce181b473a092114
Darpan Madlani <Darpan@proclink.com>
2021-11-27 21:23:40 +0530

    Junit test demo

---
+++ src/pkg/TestLogic.java
@@ -0,0 +1,17 @@
+package pkg;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestLogic {
+
+	@Test
+	public void testFindMax() {
+		int[] arr = {4,6,2,9,1,7};
+		assertEquals(9, Calculation.findMax(arr));
+		
+		int[] negArr = {-7,-3,-4,-1,-9,-19};
+		assertEquals(-1, Calculation.findMax(negArr));
+	}
+}
